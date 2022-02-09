043b71346526ddbf6f6a822838d061789954429e
Jeremias Storm <jerestorm@gmail.com>
2022-02-09 20:24:17 -0300

    finish layout

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Todo = ({todo, todoDelete, todoToggleCompleted, setTodoEdit}) =>{
     return (
-        <div className={`card border-0 mt-2 ${todo.completed && 'bg-dark text-light'}`}>
+        <div className={`card border-0 mt-2 ${todo.completed ? 'bg-info text-dark' : 'bg-dark text-light'}`}>
             <div className='card-body'>
                 <div className='d-flex justify-content-between'>
-                    <h3 className='card-title text-right'>
+                    <h3 className={`card-title text-right ${todo.completed && 'text-decoration-line-through'}`}>
                         {todo.title}
                     </h3>
                     <button 
@@ -14,22 +14,33 @@ const Todo = ({todo, todoDelete, todoToggleCompleted, setTodoEdit}) =>{
                     <i className="bi bi-check2-circle"></i>
                     </button>
                 </div>
-                <p className='card-text text-start'>
+                <p className={`card-text text-start ${todo.completed && 'text-decoration-line-through'}`}>
                     {todo.description}
                 </p>
                 < hr />
-                <div className='d-flex justify-content-end'>
-                    <button 
+                <div className='d-flex justify-content-between'>
+                    
+                    {
+                        todo.completed 
+                        ? (
+                            <p className='card-text fst-italic'>Completed</p>
+                        )
+                        : (
+                            <p className='card-text'>Incompleted</p>
+                        )
+                    }
+                    <div>
+                        <button 
                         onClick={() => setTodoEdit(todo)}
                         className='btn btn-sm btn-primary me-2'>
                             <i className="bi bi-pen"></i>
-                    </button>
-                    <button 
-                        onClick={() => todoDelete(todo.id)}
-                        className='btn btn-sm btn-danger'>
-                            <i className="bi bi-trash-fill"></i>
-                    </button>
-
+                        </button>
+                        <button 
+                            onClick={() => todoDelete(todo.id)}
+                            className='btn btn-sm btn-danger'>
+                                <i className="bi bi-trash-fill"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <div className='text-center mt-5 '>
-        <h1>To do Application</h1>
+    <div className='text-center mt-5'>
+        <h1 className=' text-light'>To do App</h1>
         < App />
     </div>,
   
